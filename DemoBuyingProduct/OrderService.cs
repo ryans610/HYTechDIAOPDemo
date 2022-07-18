37457188944967cee34775e923722a628f38932e
ryans610 <ryans610@gmail.com>
2022-07-18 15:35:22 +0800

    重構-意圖

--- DemoBuyingProduct/OrderService.cs
+++ DemoBuyingProduct/OrderService.cs
@@ -17,16 +17,7 @@ public class OrderService
         Guid productId,
         int quantity)
     {
-        // check is valid user
-        var userHttpClient = new HttpClient
-        {
-            BaseAddress = new Uri("http://my-users"),
-        };
-        var userResponse = await userHttpClient.PostAsJsonAsync(
-            "api/isUserValid",
-            new { userId });
-        userResponse.EnsureSuccessStatusCode();
-        if (!await userResponse.Content.ReadAsAsync<bool>())
+        if (!await IsUserValidAsync(userId))
         {
             throw new UserInvalidException
             {
@@ -35,37 +26,24 @@ public class OrderService
         }
 
         // check reserve product is enough
-        await using var connection = new SqlConnection("my connection string");
-        await connection.OpenAsync();
-        int reserve = await connection.ExecuteScalarAsync<int>(
-            "spGetProductReserveCount",
-            new { productId },
-            commandType: CommandType.StoredProcedure);
+        int reserve = await GetProductReserveAsync(productId);
         if (reserve < quantity)
         {
-            // notify manager
-            var message = new MimeMessage();
-            message.From.Add(MailboxAddress.Parse("from email"));
-            message.To.Add(MailboxAddress.Parse("manager's email"));
-            message.Subject = "庫存不足";
-            message.Body = new TextPart("html")
-            {
-                Text = $"商品{productId}庫存不足",
-            };
-            using var smtpClient = new SmtpClient();
-            await smtpClient.ConnectAsync("my smtp host");
-            await smtpClient.SendAsync(message);
-            await smtpClient.DisconnectAsync(true);
+            await NotifyManagerAsync(productId);
 
             return false;
         }
 
         // save order
-        var orderId = await connection.ExecuteScalarAsync<Guid>(
-            "spSaveOrder",
-            new { userId, productId, quantity },
-            commandType: CommandType.StoredProcedure);
+        var orderId = await SaveOrderAsync(userId, productId, quantity);
 
+        await NotifyLogisticForShippingProduct(orderId);
+
+        return true;
+    }
+
+    private async Task NotifyLogisticForShippingProduct(Guid orderId)
+    {
         // notify logistic for shipping product
         var logisticHttpClient = new HttpClient
         {
@@ -75,8 +53,63 @@ public class OrderService
             "api/shipping",
             new { orderId });
         logisticResponse.EnsureSuccessStatusCode();
+    }
 
-        return true;
+    private async Task<Guid> SaveOrderAsync(Guid userId, Guid productId, int quantity)
+    {
+        await using var connection = await OpenConnectionAsync();
+        return await connection.ExecuteScalarAsync<Guid>(
+            "spSaveOrder",
+            new { userId, productId, quantity },
+            commandType: CommandType.StoredProcedure);
+    }
+
+    private async Task NotifyManagerAsync(Guid productId)
+    {
+        // notify manager
+        var message = new MimeMessage();
+        message.From.Add(MailboxAddress.Parse("from email"));
+        message.To.Add(MailboxAddress.Parse("manager's email"));
+        message.Subject = "庫存不足";
+        message.Body = new TextPart("html")
+        {
+            Text = $"商品{productId}庫存不足",
+        };
+        using var smtpClient = new SmtpClient();
+        await smtpClient.ConnectAsync("my smtp host");
+        await smtpClient.SendAsync(message);
+        await smtpClient.DisconnectAsync(true);
+    }
+
+    private async Task<int> GetProductReserveAsync(Guid productId)
+    {
+        await using var connection = await OpenConnectionAsync();
+        return await connection.ExecuteScalarAsync<int>(
+            "spGetProductReserveCount",
+            new { productId },
+            commandType: CommandType.StoredProcedure);
+    }
+
+    private async Task<SqlConnection> OpenConnectionAsync()
+    {
+        var connection = new SqlConnection("my connection string");
+        await connection.OpenAsync();
+        return connection;
+    }
+
+    private async Task<bool> IsUserValidAsync(Guid userId)
+    {
+        // check is valid user
+        var userHttpClient = new HttpClient
+        {
+            BaseAddress = new Uri("http://my-users"),
+        };
+        var userResponse = await userHttpClient.PostAsJsonAsync(
+            "api/isUserValid",
+            new { userId });
+        userResponse.EnsureSuccessStatusCode();
+        var isUserValid = await userResponse.Content.ReadAsAsync<bool>();
+        return isUserValid;
     }
 }
 
