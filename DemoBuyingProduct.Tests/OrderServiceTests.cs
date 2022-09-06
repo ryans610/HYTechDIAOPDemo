c842f4a6011b16b1f0e518b9112b026c1481ba06
ryans610 <ryans610@gmail.com>
2022-09-06 18:10:47 +0800

    Demo功能

--- DemoBuyingProduct.Tests/OrderServiceTests.cs
+++ DemoBuyingProduct.Tests/OrderServiceTests.cs
@@ -2,6 +2,9 @@ namespace DemoBuyingProduct.Tests;
 
 public class OrderServiceTests
 {
+    private readonly Guid _userId = new("{CA6B6DA3-26B2-455A-878E-AD7F15A0E6FF}");
+    private readonly Guid _productId = new("{70C8A13C-99CA-44B4-8724-703AA8004076}");
+
     [SetUp]
     public void Setup()
     {
