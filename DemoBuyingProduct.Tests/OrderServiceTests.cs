aecd64adad5001518d2ed3982ebf136762cd252b
ryans610 <ryans610@gmail.com>
2022-09-28 16:40:48 +0800

    測試-初始化

--- DemoBuyingProduct.Tests/OrderServiceTests.cs
+++ DemoBuyingProduct.Tests/OrderServiceTests.cs
@@ -6,9 +6,25 @@ public class OrderServiceTests
     private readonly Guid _userId = new("{CA6B6DA3-26B2-455A-878E-AD7F15A0E6FF}");
     private readonly Guid _productId = new("{70C8A13C-99CA-44B4-8724-703AA8004076}");
 
+    private IUser _user;
+    private IProduct _product;
+    private INotification _notification;
+    private IOrder _order;
+    private IPriceCalculator _price;
+    private ILog _log;
+
+    private OrderService _orderService;
+
     [SetUp]
     public void Setup()
     {
+        _user = Substitute.For<IUser>();
+        _product = Substitute.For<IProduct>();
+        _notification = Substitute.For<INotification>();
+        _order = Substitute.For<IOrder>();
+        _price = new StandardPriceCalculator();
+        _log = Substitute.For<ILog>();
+        _orderService = new OrderService(_user, _product, _notification, _order, _price, _log);
     }
 
 
